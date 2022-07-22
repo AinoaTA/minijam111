05f0bc598aecf1e0e3297d103bee7be814dd5aff
Daniel de Salabert <danielgdesalabert@gmail.com>
2022-07-22 12:21:21 +0200

    two fire methods

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -1,13 +1,14 @@
 using System;
 using Colors;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class Weapon : MonoBehaviour
     {
-        
         [SerializeField] private ColorsController.Color projectileColor = 0;
+        [SerializeField] private bool shootFromFirePoint;
         
         [Header("Projectiles Data")]
         [SerializeField] private GameObject projectilePrefab;
@@ -19,9 +20,11 @@ namespace Player
 
         private GameObject _coloredProjectile;
         private Renderer _coloredProjectileRenderer;
+        private Camera _mainCamera;
 
         private void Start()
         {
+            _mainCamera = Camera.main;
             _coloredProjectile = projectilePrefab;
             _coloredProjectileRenderer = _coloredProjectile.GetComponent<Renderer>();
 
@@ -44,9 +47,21 @@ namespace Player
         
         public void InstantiateProjectile()
         {
-            var projectile = Instantiate(_coloredProjectile, firePoint.position, Quaternion.identity);
+            Vector3 firePosition;
+            if (shootFromFirePoint)
+            {
+                firePosition = firePoint.position;
+            }
+            else
+            {
+                var centerScreen = new Vector3(Screen.height / 2, Screen.width / 2, 0);
+                firePosition = _mainCamera.ScreenToWorldPoint(centerScreen);
+                firePosition += _mainCamera.transform.forward;
+            }
+            
+            var projectile = Instantiate(_coloredProjectile, firePosition, Quaternion.identity);
             projectile.GetComponent<Rigidbody>().velocity = firePoint.forward * projectileSpeed;
-
+            
         }
 
         private Material GetMaterial()
