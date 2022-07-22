3c992b3508a6ce6b3717461fa9bacbff7db14922
Daniel de Salabert <danielgdesalabert@gmail.com>
2022-07-22 19:36:07 +0200

    Starting Spawn Points

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -9,6 +9,11 @@ using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Header("Initial Spawn Data")]
+    [SerializeField] private List<Transform> spawnPositionsList = new List<Transform>();
+    [SerializeField] private float spawnPositionsRadius;
+    [SerializeField] private int enemiesPerSpawnPosition;
+    
     [Header("Spawn Data")]
     [SerializeField] private GameObject player;
     [SerializeField] private float minDistance;
@@ -21,12 +26,23 @@ public class EnemySpawner : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
+        foreach (var spawnPos in spawnPositionsList)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(spawnPos.position, spawnPositionsRadius);
+        }
+        
         // Draw a yellow sphere at the transform's position
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(player.transform.position, minDistance);
         
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(player.transform.position, maxDistance);
+    }
+
+    private void Start()
+    {
+        SceneStartSpawning();
     }
 
     private void Update()
@@ -39,9 +55,40 @@ public class EnemySpawner : MonoBehaviour
             var randomPosition = RandomNavMeshLocation(randomRadius);
 
             var randomEnemy = Random.Range(0, enemyPrefabList.Count - 1);
-            Instantiate(enemyPrefabList[randomEnemy], randomPosition, quaternion.identity);
+            Instantiate(enemyPrefabList[randomEnemy], randomPosition, Quaternion.identity);
             _spawnTimer = 0f;
         }
+    }
+    
+    private void SceneStartSpawning()
+    {
+        foreach (var spawnPos in spawnPositionsList)
+        {
+            for(var i = 0; i < enemiesPerSpawnPosition; i++)
+            {
+                var randomRadius = Random.Range(0.1f, spawnPositionsRadius);
+                var randomPos = RandomSceneStartLocation(spawnPos, randomRadius);
+                var randomEnemy = Random.Range(0, enemyPrefabList.Count - 1);
+                
+                Instantiate(enemyPrefabList[randomEnemy], randomPos, Quaternion.identity);
+            }
+        }
+
+    }
+    
+    
+    private Vector3 RandomSceneStartLocation(Transform spawnPos, float radius)
+    {
+        var randomDir = Random.insideUnitSphere;
+        var randomPos = spawnPos.position + randomDir * radius;
+        
+
+        if (NavMesh.SamplePosition(randomPos, out var hit, radius, 1))
+        {
+            randomPos = hit.position;
+        }
+
+        return Vector3.Distance(hit.position, player.transform.position) < minDistance ? RandomSceneStartLocation(spawnPos, radius) : randomPos;
     }
 
     private Vector3 RandomNavMeshLocation(float radius)
