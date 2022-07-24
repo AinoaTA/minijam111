5d1164a4410150b4af6dffe9f53ee6eb23fd6f02
ManuComposer <manuelgonzalezcps@gmail.com>
2022-07-24 10:30:32 +0200

    Z

--- Assets/Scripts/UI and Menus/PauseMenu.cs
+++ Assets/Scripts/UI and Menus/PauseMenu.cs
@@ -6,16 +6,28 @@ namespace UI_and_Menus
     public class PauseMenu : MonoBehaviour
     {
         CanvasGroup canvas;
+        FMOD.Studio.Bus Music;
+        FMOD.Studio.EventInstance PauseAmb;
+        float MusicVolume = 0.8f;
+
 
         private void Awake()
         {
             canvas = GetComponent<CanvasGroup>();
+            Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+            PauseAmb = FMODUnity.RuntimeManager.CreateInstance("event:/Ambience/Pause Menu");
         }
 
+        public void MusicVolumeLevel(float newMusicVolume)
+        {
+            MusicVolume = newMusicVolume;
+        }
         private void Update()
         {
+            Music.setVolume(MusicVolume);
             if (Input.GetKeyDown(KeyCode.Escape) && !GameManager.gameManager.hudController.isGameOver)
             {
+                MusicVolume = 0f;
                 GameManager.gameManager.hudController.isPause = !GameManager.gameManager.hudController.isPause;
                 GameManager.gameManager.hudController.UnlockMouse(GameManager.gameManager.hudController.isPause);
                 if(GameManager.gameManager.hudController.isPause)
@@ -28,6 +40,7 @@ namespace UI_and_Menus
         }
         public void OnResumePressed()
         {
+            MusicVolume = 0.8f;
             GameManager.gameManager.hudController.HideCanvasGroup(canvas);
             GameManager.gameManager.hudController.Lock();
             GameManager.gameManager.hudController.isPause = false;
